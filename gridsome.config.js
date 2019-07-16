3103ae2e4d72c88312390252350e4ba25f451345
earlman <earl.mangulabnan@gmail.com>
2019-07-16 12:35:04 -0500

    Update siteName

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,38 +5,38 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-  siteName: 'Gridsome Blog Starter',
-  siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.',
+    siteName: 'Jisoo Portfolio',
+    //   siteDescription: 'A simple, hackable & minimalistic starter for Gridsome that uses Markdown for content.',
 
-  plugins: [
-    {
-      // Create posts from markdown files
-      use: '@gridsome/source-filesystem',
-      options: {
-        typeName: 'Post',
-        path: 'content/posts/*.md',
-        route: '/:slug',
-        refs: {
-          // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
-          tags: {
-            typeName: 'Tag',
-            route: '/tag/:id',
-            create: true
-          }
+    plugins: [
+        {
+            // Create posts from markdown files
+            use: '@gridsome/source-filesystem',
+            options: {
+                typeName: 'Post',
+                path: 'content/posts/*.md',
+                route: '/:slug',
+                refs: {
+                    // Creates a GraphQL collection from 'tags' in front-matter and adds a reference.
+                    tags: {
+                        typeName: 'Tag',
+                        route: '/tag/:id',
+                        create: true
+                    }
+                }
+            }
         }
-      }
-    }
-  ],
+    ],
 
-  transformers: {
-    //Add markdown support to all file-system sources
-    remark: {
-      externalLinksTarget: '_blank',
-      externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
-      anchorClassName: 'icon icon-link',
-      plugins: [
-        '@gridsome/remark-prismjs'
-      ]
-    }
-  },
+    transformers: {
+        //Add markdown support to all file-system sources
+        remark: {
+            externalLinksTarget: '_blank',
+            externalLinksRel: ['nofollow', 'noopener', 'noreferrer'],
+            anchorClassName: 'icon icon-link',
+            plugins: [
+                '@gridsome/remark-prismjs'
+            ]
+        }
+    },
 }
