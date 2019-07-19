c5a8eda9cc0bb407dec0c88d8ac1cedcb3de5cc4
earlman <earl.mangulabnan@gmail.com>
2019-07-18 19:20:21 -0500

    style buttons

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,5 +1,5 @@
 <template>
-	<Layout>
+	<Layout class="index">
 		<div slot="left">
 			<h1>what do i do?</h1>
 			<ul>
@@ -9,9 +9,11 @@
 			</ul>
 		</div>
 		<div slot="right">
-			<div class="button-large">graphic design</div>
-			<div class="button-large">instagram</div>
-			<div class="button-large">email campaign</div>
+			<div class="button-container">
+				<div class="button-large">graphic design</div>
+				<div class="button-large">instagram</div>
+				<div class="button-large">email campaign</div>
+			</div>
 			<!-- <g-image src="~/../img/cover.jpg" width="800"></g-image> -->
 		</div>
 	</Layout>
@@ -25,3 +27,37 @@ export default {
 	}
 };
 </script>
+
+<style lang="sass" scoped>
+
+.index
+    h1
+        font-weight: 700
+        color: var(--color-N400)
+
+    li
+        @include text(xl)
+        font-weight: 500
+        list-style-type: none
+        color: var(--color-N070)
+
+    ul
+        padding: 0
+
+
+.button-container
+    display: flex
+    flex-direction: column
+    align-items: center
+
+.button-large
+    @include space-stack(3rem)
+    @include space-squish(4rem)
+    @include text(xl)
+    border: 5px solid var(--color-text-b-l)
+    display: inline-block
+    border-radius: 2px
+    width: 25ch
+    text-align: center
+
+</style>
