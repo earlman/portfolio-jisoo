4618691412377a1fcbf1ea22db4bc4043686fc8b
earlman <earl.mangulabnan@gmail.com>
2019-07-22 21:15:51 -0500

    add feature image page

--- src/components/FeatureImage.vue
+++ src/components/FeatureImage.vue
@@ -1,10 +1,28 @@
 <template>
-	<div>RIGHTTTTTTTTTTT</div>
+	<div class="feature-image">
+		<img :src="image_path" :alt="alt" />
+	</div>
 </template>
 
 <script>
-export default {};
+export default {
+	props: {
+		image_path: {
+			type: String,
+			default: "/instantglam-nude-natural.jpg"
+		},
+		alt: {
+			type: String,
+			default: "feature image"
+		}
+	}
+};
 </script>
 
-<style lang="scss" scoped>
+<style lang="sass" scoped>
+
+.feature-image
+    img
+        border-radius: 3px
+
 </style>
