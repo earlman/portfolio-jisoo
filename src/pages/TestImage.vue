8e62d6cdb42ce0c5f604dc0196982311e5350e28
earlman <earl.mangulabnan@gmail.com>
2019-07-22 20:24:22 -0500

    add FeatureImage component

---
+++ src/pages/TestImage.vue
@@ -0,0 +1,19 @@
+<template>
+	<Layout>
+		<div slot="right">
+			<FeatureImage></FeatureImage>
+		</div>
+	</Layout>
+</template>
+
+<script>
+import FeatureImage from "~/components/FeatureImage";
+export default {
+	components: {
+		FeatureImage
+	}
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
