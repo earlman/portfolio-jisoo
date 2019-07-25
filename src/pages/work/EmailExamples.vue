1b38edf7c92fca1ede1422bc81ba27cae6dbfea0
earlman <earl.mangulabnan@gmail.com>
2019-07-24 22:00:59 -0500

    responsive code

--- src/pages/work/EmailExamples.vue
+++ src/pages/work/EmailExamples.vue
@@ -3,12 +3,15 @@
 		<div slot="left">
 			<button @click="changeDisplay(`weekday`)">weekday</button>
 			<button @click="changeDisplay(`weekend`)">weekend</button>
+			<div v-for="page in pages" :key="page.id" class="display-area">
+				<FeatureImage :img_path="page.img_code" v-if="page.name==current_page" />
+			</div>
 		</div>
 		<div slot="right">
 			<div v-for="page in pages" :key="page.id" class="display-area">
-                <div v-for="path in page.img_path" :key="path.id">
-				    <FeatureImage :img_path="path" v-if="page.name==current_page" />
-                </div>
+				<div v-for="path in page.img_path" :key="path.id">
+					<FeatureImage :img_path="path" v-if="page.name==current_page" />
+				</div>
 
 				<!-- <FeatureImage :img_path="page.img_path" v-if="page.name==current_page"></FeatureImage> -->
 			</div>
@@ -27,11 +30,13 @@ export default {
 			pages: [
 				{
 					name: "weekday",
-					img_path: ["/detail_page/lashes-1.jpg", "/detail_page/lashes-2.jpg"]
+					img_code: "/emails/weekdaycode.png",
+					img_path: ["/emails/weekday.png"]
 				},
 				{
 					name: "weekend",
-					img_path: ["/detail_page/lashes-2.jpg"]
+					img_code: "/emails/weekendcode.png",
+					img_path: ["/emails/weekend.png"]
 				}
 			]
 		};
