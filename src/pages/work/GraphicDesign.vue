a5b71b1d2d871ae2e2c6a56aa29e335905a7058b
earlman <earl.mangulabnan@gmail.com>
2019-07-24 22:27:58 -0500

    space out images

--- src/pages/work/GraphicDesign.vue
+++ src/pages/work/GraphicDesign.vue
@@ -10,8 +10,10 @@
 			</button>
 		</div>
 		<div slot="right">
-			<div v-for="path in images" :key="path.id">
-				<FeatureImage :img_path="path" />
+			<div class="img_container">
+				<div v-for="path in images" :key="path.id">
+					<FeatureImage :img_path="path" />
+				</div>
 			</div>
 		</div>
 	</Layout>
@@ -39,7 +41,9 @@ export default {
 </script>
 
 <style lang="sass" scoped>
-.p
-    display: grid
-    height: 2000px
+
+.img_container
+    &>*
+        @include space-stack(var(--space-md))
+
 </style>
